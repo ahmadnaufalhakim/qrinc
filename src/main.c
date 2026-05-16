e964e0c537c2dfe7beed770ce99f28dffdf6afd0
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2026-05-16 14:25:40 +0700

    add main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-    printf("Hello, world!\n");
+#include "qr_encoding.h"
+
+int main(int argc, char* argv[]) {
+    const char* str = "HELLO WORLD";
+    size_t len = strlen(str);
+    size_t count;
+
+    char (*encoded)[12] = qr_alphanumeric_mode_encode(str, len, &count);
+    if (!encoded) {
+        fprintf(stderr, "encoding failed\n");
+        return 1;
+    }
+
+    printf("Input: %s\n\n", str);
+
+    for (size_t i = 0; i < count; i++) {
+        printf("chunk %zu: %s\n", i, encoded[i]);
+    }
+
+    free(encoded);
+
     return 0;
 }
